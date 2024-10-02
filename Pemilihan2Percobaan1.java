cee79606f3864cb6b6b284c4f46c832aaa708c87
muhammadrayhan11 <rayhanzmzmi03@gmail.com>
2024-10-02 22:06:21 +0700

    add file:percobaan1

---
+++ Pemilihan2Percobaan1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1 {
+
+    public static void main(String[] args) {
+        Scanner tujuhBelas = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan Tahun : ");
+        tahun = tujuhBelas.nextInt();
+
+        if ((tahun % 4 ) == 0) {
+            if ((tahun % 100) != 0) 
+                System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
