c3de36759408b2f6bcc9efc00fa27257e12c24be
unknown <rayymekdi@gmail.com>
2024-10-03 16:11:08 +0700

    change:pertanyaanPercobaan1

--- Pemilihan2Percobaan1_17.java
+++ Pemilihan2Percobaan1_17.java
@@ -13,6 +13,11 @@ public class Pemilihan2Percobaan1_17 {
         if ((tahun % 4 ) == 0) {
             if ((tahun % 100) != 0) 
                 System.out.println("Tahun Kabisat");
+            else 
+                if ((tahun % 400) == 0) 
+                    System.out.println("Tahun Kabisat");
+                else 
+                    System.out.println("Bukan Tahun Kabisat");
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
